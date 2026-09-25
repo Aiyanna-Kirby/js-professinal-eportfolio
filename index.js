7ac47f120577d562922e07c324a600d63db39f5c
Aiyanna Smith-Taylor <aiyannast@outlook.com>
2026-09-24 19:29:41 -0500

    js email link; complete.

---
+++ index.js
@@ -0,0 +1,27 @@
+/* EMAIL CONNECT */
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_5nkjjjq",
+      "template_bo5469r",
+      event.target,
+      "gOnJO2ySsNliAPJWj",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavaible. Please contact me directly at aiyannast@outlook.com",
+      );
+    });
+}
+
+/* MODAL TOGGLE */
